bb56c65b24a4f983f06fd38565d349f2fb8d9fc1
CarlaOnate <carlaonategardella@gmail.com>
2022-10-13 13:25:46 -0500

    uploading evidnecia 1 from rpelit

--- Trie/graph.cpp
+++ Trie/graph.cpp
@@ -4,13 +4,14 @@
 
 #include "graph.h"
 
-
-TRIE::TRIE() {
+// Funcion para inicializar un objeti tipo TRIE
+TRIE::TRIE() { // O(1)
   // Inicializar el root a un nodo vacio
   this->root = new Node();
 }
 
-void TRIE::addText(string text) {
+// Funcion que recibe el texto a agregar y lo cicla para irlo agregando en el arbol TRIE
+void TRIE::addText(string text) { // O(n)
   Node* currentNode = this->root;
   for (int i  = 0; i < text.length(); i++) {
     while (currentNode->nextNodes[text[i]]) { // Mientras haya camino siguelo
@@ -25,7 +26,8 @@ void TRIE::addText(string text) {
   currentNode->endNode = true;
 }
 
-bool TRIE::searchTrie(string text) {
+// Funcion que recibe el texto a buscar lo cicla, con cada caracter va avanznando en el arbol TRIE hasta encontrar o no encontrar la palabra, resera un bool
+bool TRIE::searchTrie(string text) { // O(n)
   cout << "texto a buscar: " << text << "\n";
   Node* currentNode = this->root;
   string resultingSearch;
@@ -36,18 +38,19 @@ bool TRIE::searchTrie(string text) {
     resultingSearch += text[i];
     i++;
   }
-  //cout << "Caracteres encontrados: " << resultingSearch << " => BOOL:  ";
-  cout << " => BOOL:  ";
-  if (currentNode->endNode)
+  cout << " => Encontrado:  ";
+  if (currentNode->endNode && resultingSearch == text)
     return true;
   return false;
 }
 
-void TRIE::getGraph(Node* root, int level, map<int, vector<char>> &mapTrie) {
+// Funcion recursiva que recibe el nodo actual, nivel dentro del arbol y un map para guardar los valores del arbol por nivel.
+// Sigue todos los caminos posibles dentro del arbol
+void TRIE::getGraph(Node* root, int level, map<int, vector<char>> &mapTrie) { // O(branches^depth)
     for (std::pair<const char, Node*> el : root->nextNodes){
       mapTrie[level].push_back(el.first);
       cout << " " << el.first << " ";
-      if (!el.second->endNode){
+      if (!el.second->nextNodes.empty()){
         getGraph(el.second, level + 1, mapTrie);
       } else {
         cout << " $\n";
@@ -55,7 +58,8 @@ void TRIE::getGraph(Node* root, int level, map<int, vector<char>> &mapTrie) {
     }
 }
 
-void TRIE::printResultingTrie(map<int, vector<char>> mapTrie) {
+// Funcion que recibe un map y lo cicla para imprimir los valores que tiene dentro
+void TRIE::printResultingTrie(map<int, vector<char>> mapTrie) { // O(n*m)
   for (pair<const int, vector<char>> level : mapTrie) {
       cout << "Nivel (" << level.first << "):  ";
       for (auto character : level.second) {
--- Trie/trie.cpp
+++ Trie/trie.cpp
@@ -17,6 +17,7 @@ bool regexNumber(string numb){
   return regex_match(numb, expReg);
 }
 
+// Funcion que recibe un string y lo cicla para validar que sea un digito, regresa un bool
 bool validateDigit(string digit) {
   for (auto character : digit) {
     if (!isdigit(character)) return false;
@@ -26,68 +27,69 @@ bool validateDigit(string digit) {
 
 int main () {
   TRIE graph;
-  graph.addText("abbc");
-  graph.addText("acdf");
-  graph.addText("holoo");
-  graph.addText("halos");
-  cout << "TRIE: \n";
-  //map<int, vector<char>> resultingTrie;
   map<int, vector<char>> resultingTrie;
+  int nNumber = 0, mNumber = 0, i = 0;
+  string n, m;
+//  graph.addText("tosana");
+//  graph.addText("torta");
+//  graph.addText("taco");
+//  graph.addText("topo");
+//  graph.addText("topotastico");
+//  graph.getGraph(graph.root, 0, resultingTrie);
+//  graph.printResultingTrie(resultingTrie);
+
+  cout<<"\nBienvenido a TRIE. \n";
+  cout<<"Ingresa el numero de palabras a insertar: \n";
+  cin >> n;
+
+  if (!validateDigit(n)) {
+    cout<<"ERROR: No es numero \n";
+    exit(0);
+  }
+  nNumber = stoi(n);
+  cout << nNumber << "\n";
+  while(i < nNumber) {
+    string word;
+    cout<<"Ingresa la palabra ("<<(i + 1)<<"): \n";
+    cin.ignore();
+    cin >> word;
+    if(regexWord(word) == true) {
+      graph.addText(word);
+      i++;
+    } else {
+      cout<<"ERROR: Solo se aceptan caracteres de la a - z, minusculas y sin espacios.\n";
+    }
+  }
+  cout<<"\n";
+
+  // Mostrar TRIE generado
+  cout << "TRIE: \n";
   graph.getGraph(graph.root, 0, resultingTrie);
   graph.printResultingTrie(resultingTrie);
-  cout << graph.searchTrie("abbc") << "\n";
-  cout << graph.searchTrie("acdf")  << "\n";
-  cout << graph.searchTrie("hol")  << "\n";
-  cout << graph.searchTrie("halos") << "\n";
-  cout << graph.searchTrie("gato") << "\n";
-//  int i = 0;
-//  string n, m;
-//  int mNumber, nNumber;
-//  cout<<"\nBienvenido a TRIE. \n";
-//  cout<<"Ingresa el numero de palabras a insertar: \n";
-//  cin >> n;
-//
-//  if (!validateDigit(n)) {
-//    cout<<"ERROR: No es numero \n";
-//    nNumber = stoi(n);
-//  }
-//
-//  while(i < nNumber) {
-//    string word;
-//    cout<<"Ingresa la palabra ("<<(i + 1)<<"): \n";
-//    cin.ignore();
-//    getline(cin, word);
-//    if(regexWord(word) == true) {
-//      graph.addText(word);
-//      i++;
-//    } else {
-//      cout<<"ERROR: Solo se aceptan caracteres de la a - z, minusculas y sin espacios.\n";
-//    }
-//  }
-//  cout<<"\n";
-//
-//  cout<<"Ingresa el numero de palabras a buscar: ";
-//  cin >> m;
-//
-//  if (!validateDigit(m)) {
-//    cout<<"ERROR: No es numero \n";
-//    mNumber = stoi(m);
-//  }
-//
-//  i = 0;
-//  while(i < mNumber) {
-//    string word;
-//    cin>>word;
-//
-//    if (regexWord(word)) {
-//      cout << "Encontrado: " << "\n";
-//      graph.searchTrie(word);
-//      graph.printGraph(graph.root);
-//      i++;
-//    } else {
-//      cout<<"ERROR: Solo se aceptan caracteres de la a - z. \n";
-//    }
-//    cout<<"\n";
-//  }
+
+  cout<<"Ingresa el numero de palabras a buscar: ";
+
+  cin.ignore(INT_MAX, '\n');
+  cin >> m;
+
+  if (!validateDigit(m)) {
+    cout<<"ERROR: No es numero \n";
+    exit(0);
+  }
+  mNumber = stoi(m);
+  cout << mNumber << "\n";
+  i = 0;
+  while(i < mNumber) {
+    string word;
+    cin>>word;
+
+    if (regexWord(word)) {
+      (graph.searchTrie(word)) ? (cout << "VERDADERO \n") : (cout << "FALSO \n");
+      i++;
+    } else {
+      cout<<"ERROR: Solo se aceptan caracteres de la a - z. \n";
+    }
+    cout<<"\n";
+  }
 }
 
