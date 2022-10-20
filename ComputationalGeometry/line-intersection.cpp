3381a57cd258bf134d315dd4c23c9fd013670cd3
CarlaOnate <carlaonategardella@gmail.com>
2022-10-20 12:50:58 -0500

    intersect functions done

--- ComputationalGeometry/line-intersection.cpp
+++ ComputationalGeometry/line-intersection.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,25 +29,44 @@ vector<Line> getData () {
   return lines;
 };
 
-int orientation (pair<int, int> point1, pair<int, int> point2, pair<int, int> point3) {
-  int orientation = (point3.first - point1.first) * (point2.second - point1.second) - (point2.first - point1.first) * (point2.second - point1.second);
+bool checkSegment (pair<int, int> point1, pair<int, int> point2, pair<int, int> point3) {
+  if ( point3.first <= max (point1.first, point2.first) && point3.first >= min (point1.first, point2.first) &&
+       point3.second <= max (point1.second, point2.second) && point3.second >= min (point1.second, point2.second) ) {
+    return true;
+  }
+  return false;
+}
 
-  cout << "\n\t orientation : " << orientation << "\n";
+int orientation (pair<int, int> point1, pair<int, int> point2, pair<int, int> point3) {
+  int orientation = ((point3.first - point1.first) * (point2.second - point1.second)) - ((point2.first - point1.first) * (point3.second - point1.second));
 
   if (orientation == 0) return 0;
   if (orientation > 0) return 1;
     return 2;
 }
 
+bool intersect (Line line1, Line line2) {
+  int line1toLine2_p1_Orientation = orientation(line1.p1, line1.p2, line2.p1);
+  int line1toLine2_p2_Orientation = orientation(line1.p1, line1.p2, line2.p2);
+  int line2toLine1_p1_Orientation = orientation(line2.p1, line2.p2, line1.p1);
+  int line2toLine1_p2_Orientation = orientation(line2.p1, line2.p2, line1.p2);
+
+  if (line1toLine2_p1_Orientation != line1toLine2_p2_Orientation && line2toLine1_p1_Orientation != line2toLine1_p2_Orientation)
+    return true;
+
+  if (line1toLine2_p1_Orientation == 0 && checkSegment (line1.p1, line1.p2, line2.p1)) return true;
+  if (line1toLine2_p2_Orientation == 0 && checkSegment (line1.p1, line1.p2, line2.p2)) return true;
+  if (line2toLine1_p1_Orientation == 0 && checkSegment (line2.p1, line2.p2, line1.p1)) return true;
+  if (line2toLine1_p2_Orientation == 0 && checkSegment (line2.p1, line2.p2, line1.p2)) return true;
+
+  return false;
+};
 
 int main () {
  //vector<Line> lines = getData();
- Line line1 = Line({0, 0}, {2, 1});
- Line line2 = Line({-1, -1}, {1, -1});
+ Line line1 = Line({0, 0}, {1, 1});
+ Line line2 = Line({-2, 2}, {1, 0});
 
- int line1toLine2_p1_Orientation = orientation(line1.p1, line1.p2, line2.p1);
- int line1toLine2_p2_Orientation = orientation(line1.p1, line1.p2, line2.p2);
-
- cout << " line 1 to line 2 p1 " << line1toLine2_p1_Orientation <<"\n";
- cout << " line 1 to line 2 p2 " << line1toLine2_p2_Orientation <<"\n";
+ bool intersectan = intersect(line1, line2);
+ cout << "\n" << intersectan << "\n";
 };
