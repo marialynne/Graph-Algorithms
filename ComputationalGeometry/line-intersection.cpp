8974b80e9f67282ac2887985bb61d6107d9aec9e
CarlaOnate <carlaonategardella@gmail.com>
2022-10-24 10:03:30 -0500

    line-intersection finished without inputs and validations

--- ComputationalGeometry/line-intersection.cpp
+++ ComputationalGeometry/line-intersection.cpp
@@ -3,18 +3,20 @@
 #include <utility>
 #include <algorithm>
 #include <set>
+#include <map>
+#include <string>
 using namespace std;
 
 class Coordinate
 {
 public:
   pair<float, float> point;
-  char identifier;
+  string identifier;
 
   Coordinate()
   {
     this->point = {0, 0};
-    this->identifier = ' ';
+    this->identifier = " ";
   }
 
   Coordinate(pair<float, float> _point, char _identifier)
@@ -53,40 +55,38 @@ class Graph
 public:
   vector<Line> segments;
   vector<Line> segmentsToCheck;
-  vector<int> setps;
-  set<pair<Line, Line>> intersections;
+  vector<int> steps;
+  map<string, set<string>> checkedLines;
 
   void getSteps()
   {
     for (auto i : this->segments)
     {
-      this->setps.push_back(i.p1.point.second);
-      this->setps.push_back(i.p2.point.second);
+      this->steps.push_back(i.p1.point.second);
+      this->steps.push_back(i.p2.point.second);
     }
-    sort(this->setps.begin(), this->setps.end(), [](const int &a, const int &b) -> bool
+    sort(this->steps.begin(), this->steps.end(), [](const int &a, const int &b) -> bool
          { return a > b; });
 
-    vector<int>::iterator it = unique(this->setps.begin(), this->setps.end());
+    vector<int>::iterator it = unique(this->steps.begin(), this->steps.end());
 
-    this->setps.resize(distance(this->setps.begin(), it));
+    this->steps.resize(distance(this->steps.begin(), it));
   }
 
-  void searchForIntersection()
-  {
-    if (this->segmentsToCheck.size() <= 1)
-      return;
-
-    for (int line1 = 0; line1 < segmentsToCheck.size(); line1++)
-    {
-      for (int line2 = 0; line2 < segmentsToCheck.size(); line2++)
-      {
-        if (line1 != line2)
-        {
-          // if (intersect(this->segmentsToCheck[line1], this->segmentsToCheck[line2]))
+  void searchForIntersection() {
+    for (int line1 = 0; line1 < segmentsToCheck.size(); line1++) {
+      for (int line2 = 0; line2 < segmentsToCheck.size(); line2++) {
+        if (line1 != line2) {
           Line tempLine1 = this->segmentsToCheck[line1];
           Line tempLine2 = this->segmentsToCheck[line2];
-          cout << tempLine1.p1.identifier << tempLine1.p2.identifier << " " << tempLine2.p1.identifier << tempLine2.p2.identifier << " " << intersect(tempLine1, tempLine2) << endl;
-          // this->intersections.insert(make_pair(tempLine1, tempLine2));
+          string line1_id = tempLine1.p1.identifier + tempLine1.p2.identifier;
+          string line2_id = tempLine2.p1.identifier + tempLine2.p2.identifier;
+
+          if (checkedLines[line1_id].find(line2_id) == checkedLines[line1_id].end()) {
+            checkedLines[line1_id].insert(line2_id);
+            checkedLines[line2_id].insert(line1_id);
+            cout << tempLine1.p1.identifier << tempLine1.p2.identifier << " " << tempLine2.p1.identifier << tempLine2.p2.identifier << " " << intersect(tempLine1, tempLine2) << endl;
+          }
         }
       }
     }
@@ -95,26 +95,30 @@ public:
   void sweepingLine()
   {
     getSteps();
+    set<string> linesWithoutComparison; // Lines que no se comparan porque estan solas en esa parte del sweeping line.
+    cout << "\nIntersections: " << endl;
 
-    for (int stepAtY : this->setps)
-    {
-      for (auto line : this->segments)
-      {
-        if (line.p1.point.second == stepAtY)
-        {
+    for (int stepAtY : this->steps) {
+      for (int index = 0; index < this->segments.size(); index++) {
+        Line line = this->segments[index];
+        string line_id = line.p1.identifier + line.p2.identifier;
+
+        if (line.p1.point.second == stepAtY) {
           this->segmentsToCheck.push_back(line);
           searchForIntersection();
         }
-      }
-
-      for (auto line : this->segments)
-      {
-        if (line.p2.point.second == stepAtY)
-        {
-          vector<Line>::iterator it = find(this->segmentsToCheck.begin(), this->segmentsToCheck.end(), line);
-          this->segmentsToCheck.erase(it);
+        if (line.p2.point.second > stepAtY) {
+          vector<Line>::iterator it;
+          it = find_if(this->segmentsToCheck.begin(), this->segmentsToCheck.end(), [line](Line const& nextLine){ return nextLine.p1.identifier == line.p1.identifier && nextLine.p2.identifier == line.p2.identifier; });
+          if (it != this->segmentsToCheck.end()) this->segmentsToCheck.erase(it);
+          if (checkedLines[line_id].empty()) linesWithoutComparison.insert(line_id);
         }
+        if (line.p2.point.second == stepAtY && checkedLines[line_id].empty()) linesWithoutComparison.insert(line_id);
       }
+    }
+
+    for (string line : linesWithoutComparison) {
+      cout << line << " No se intersecta con ninguna linea \n";
     }
   }
 
@@ -122,7 +126,7 @@ public:
   {
     this->segments.push_back(Line(point1, point1Identifier, point2, point2Identifier));
 
-    sort(this->segments.begin(), this->segments.end(), [](const Line &a, const Line &b) -> bool
+    sort(this->segments.begin(), this->segments.end(), [](Line &a, Line &b) -> bool
          { return a.p1.point.second > b.p1.point.second; });
   }
 
@@ -131,13 +135,6 @@ public:
     cout << "\nYour given lines are: " << endl;
     for (auto i : segments)
       cout << i.p1.identifier << ": (" << i.p1.point.first << ", " << i.p1.point.second << ")\t" << i.p2.identifier << ": (" << i.p2.point.first << ", " << i.p2.point.second << ") " << endl;
-  }
-
-  void print(set<pair<Line, Line>> intersections)
-  {
-    cout << "\nIntersections: " << endl;
-    for (auto i : intersections)
-      cout << i.first.p1.identifier << i.first.p2.identifier << " -> " << i.second.p1.identifier << i.second.p2.identifier << ": TRUE" << endl;
   }
 
   bool checkSegment(pair<int, int> point1, pair<int, int> point2, pair<int, int> point3)
@@ -196,13 +193,15 @@ int main()
 {
 
   Graph myGraph;
-  myGraph.insertLine({0, 0}, 'A', {1, 1}, 'B');
-  myGraph.insertLine({-2, 2}, 'C', {1, 0}, 'D');
-  myGraph.insertLine({-6, 2}, 'F', {-4, 4}, 'E');
-  myGraph.insertLine({0, 2}, 'H', {-2, 6}, 'G');
-  myGraph.insertLine({2, 4}, 'K', {4, 0}, 'L');
-  myGraph.insertLine({2, 2}, 'I', {4, 4}, 'J');
+  myGraph.insertLine({-6, 4}, 'A', {-5, 0}, 'B');
+  myGraph.insertLine({-1, 0}, 'C', {-4, 2}, 'D');
+  myGraph.insertLine({-6, -4}, 'E', {-4, -2}, 'F');
+  myGraph.insertLine({-5, 7}, 'G', {-4, 4}, 'H');
+  myGraph.insertLine({1, 7}, 'I', {-6, 3}, 'J');
+  myGraph.insertLine({-2, 6}, 'K', {1, 3}, 'L');
+  myGraph.insertLine({2, -1}, 'M', {2, 2}, 'N');
+  myGraph.insertLine({2, 1}, 'O', {2, 4}, 'P');
+  myGraph.insertLine({-2, 12}, 'Q', {-2, 10}, 'R');
   myGraph.print(myGraph.segments);
-  myGraph.print(myGraph.intersections);
   myGraph.sweepingLine();
 };
