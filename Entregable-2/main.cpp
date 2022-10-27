bf757476c1f9adcefbb5dd21105f2220bdaec091
CarlaOnate <carlaonategardella@gmail.com>
2022-10-27 12:52:29 -0500

    maxFlow problem

--- Entregable-2/main.cpp
+++ Entregable-2/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <vector>
-#include "dijkstra.h"
-#include "maximumFlow.h"
-#include "travelingSalesman.h"
+//#include "djikstra.h"
+#include "./Include/maximumFlow.h"
+//#include "travelingSalesman.h"
 
 using namespace std;
 
@@ -13,16 +13,32 @@ int main()
                                        {45, 18, 0, 7},
                                        {32, 21, 7, 0}};
 
-    vector<vector<int>> secondMatrix = {{0, 48, 12, 18},
-                                        {52, 0, 42, 32},
-                                        {18, 46, 0, 56},
-                                        {24, 36, 52, 0}};
+//    vector<vector<float>> secondMatrix = {{0, 48, 12, 18},
+//                                        {52, 0, 42, 32},
+//                                        {18, 46, 0, 56},
+//                                        {24, 36, 52, 0}};
+
+//    vector<vector<float>> secondMatrix = {{0, 10, 8, 5, 0, 0, 0},
+//                                         {0, 0, 7, 0, 6, 0, 0},
+//                                         {0, 0, 0, 4, 0, 3, 0},
+//                                         {0, 0, 0, 0, 0, 10, 0},
+//                                         {0, 0, 0, 0, 0, 8, 2},
+//                                         {0, 0, 0, 0, 0, 0, 9},
+//                                         {0, 0, 0, 0, 0, 0, 0}};
+//
+    vector<vector<float>> secondMatrix = {{0, 10, 0, 10, 0, 0},
+                                         {0, 0, 4, 2, 8, 0, 0},
+                                         {0, 0, 0, 4, 0, 3, 10},
+                                         {0, 0, 0, 9, 0, 10, 0},
+                                         {0, 0, 0, 0, 0, 8, 10},
+                                         {0, 0, 0, 0, 0, 0, 0}};
 
     vector<pair<int, int>> centrals = {{200, 500},
                                        {300, 100},
                                        {450, 150},
                                        {520, 480}};
 
-    everyNodeDijkstra(convertDataToEdges(firstMatrix));
+    // everyNodeDijkstra(convertDataToEdges(firstMatrix));
+    maxFlow(secondMatrix, secondMatrix.size());
     return 0;
 }
